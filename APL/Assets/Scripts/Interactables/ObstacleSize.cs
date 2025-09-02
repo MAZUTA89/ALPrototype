d434e52c9ecd6e0132325b5c64cf38df80c24c2c
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2025-09-02 03:59:47 +0300

    + install size code

---
+++ APL/Assets/Scripts/Interactables/ObstacleSize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ALP.Interactables
+{
+    public class ObstacleSize
+    {
+        public SizeType SizeType { get; private set; }
+        public Vector2 Size { get; private set; }
+
+        protected Vector3 CenterPosition { get; private set; }
+
+        public ObstacleSize(Vector2 size, SizeType sizeType, IObstacle obstacle)
+        {
+            Size = size;
+            SizeType = sizeType;
+        }
+
+
+    }
+
+    public enum SizeType
+    {
+        Cube,
+        L
+    }
+}
