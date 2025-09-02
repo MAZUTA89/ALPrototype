65e50f8aabfd00e7576afce26655621e5cac08d1
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2025-09-02 04:35:18 +0300

    +

--- APL/Assets/Scripts/Interactables/ObstacleSize.cs
+++ APL/Assets/Scripts/Interactables/ObstacleSize.cs
@@ -8,21 +8,20 @@ namespace ALP.Interactables
     {
         public SizeType SizeType { get; private set; }
         public Vector2 Size { get; private set; }
-
         protected Vector3 CenterPosition { get; private set; }
-
         public ObstacleSize(Vector2 size, SizeType sizeType, IObstacle obstacle)
         {
             Size = size;
             SizeType = sizeType;
         }
-
-
     }
 
     public enum SizeType
     {
-        Cube,
-        L
+        Single,
+        Horizontal2,
+        Vertical2,
+        Square2x2,
+        LShape
     }
 }
