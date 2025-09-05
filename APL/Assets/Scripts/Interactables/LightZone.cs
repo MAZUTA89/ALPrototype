49775f8e20b1f040f766129255fe7dbb71a78003
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2025-09-06 00:14:00 +0300

    +

--- APL/Assets/Scripts/Interactables/LightZone.cs
+++ APL/Assets/Scripts/Interactables/LightZone.cs
@@ -5,23 +5,8 @@ using UnityEngine;
 
 namespace ALP.Interactables
 {
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(Collider))]
     public class LightZone : MonoBehaviour
     {
-        Collider _collider;
-        Rigidbody _rb;
-
-        #region UnityMethods
-        private void Start()
-        {
-            _collider = GetComponent<Collider>();
-            _collider.isTrigger = true;
-            _rb = GetComponent<Rigidbody>();
-            _rb.isKinematic = true;
-        }
-        #endregion
-
         public void OnEnterObstacle()
         {
             Destroy(gameObject);
